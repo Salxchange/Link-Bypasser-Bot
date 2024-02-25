271b21e549438d85903d9972441586a80ce048ff
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-25 12:52:25 +0100

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,6 +1,6 @@
 
 
-HELP_TEXT = f'<b>
+   HELP_TEXT = """<b>
 🤖 𝖬𝗒 𝗇𝖺𝗆𝖾: <a href=http://t.me/FZXFileStreamBot>𝖥𝗂𝗅𝖾 𝖲𝗍𝗋𝖾𝖺𝗆 𝖲𝗍𝗈𝗋𝖾</a>
 📚 𝖫𝗂𝖻𝗋𝖺𝗋𝗒: <a href=https://github.com/pyrogram>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆</a>
 🚀 𝖲𝖾𝗋𝗏𝖾𝗋 : <a href=https://www.Heroku.com>Heroku</a>
@@ -8,4 +8,4 @@ HELP_TEXT = f'<b>
 💾 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾: <a href=https://cloud.mongodb.com>𝖬𝗈𝗇𝗀𝗈𝖣𝖡</a>
 🧑‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: <a href=https://telegram.me/MysterySD>꧁❏รเlєภt ๔є๓๏ภ❏꧂</a>
 
-♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓</a> </b>'
+♻️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒: <a href=https://telegram.me/Rokubotz>𝖱𝗈𝗄𝗎𝖻𝗈𝗍𝗓</a> </b>"""
