49933044ab92dc940dac060dacbc961d0127b09c
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-25 12:43:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def loopthread(message,otherss=False):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am a Terabox Link Bypass Bot, just send me any terabox links and i will help you bypass it\ni also support bypassing some shortlinks sites__",
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am a Terabox Link Bypass Bot, just send me any terabox link and i will help you bypass it\ni also support bypassing links from some shortlinks sites__",
     reply_markup=InlineKeyboardMarkup(
                 [
                     [
