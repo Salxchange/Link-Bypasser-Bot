83acc817ea5dd63519071415f44be1cb7f694540
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-25 12:58:12 +0100

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,7 +1,7 @@
 HELP_TEXT = """<b>
-🤖 𝖬𝗒 𝗇𝖺𝗆𝖾: <a href=http://t.me/FZXFileStreamBot>𝖥𝗂𝗅𝖾 𝖲𝗍𝗋𝖾𝖺𝗆 𝖲𝗍𝗈𝗋𝖾</a>
+🤖 𝖬𝗒 𝗇𝖺𝗆𝖾: <a href=http://t.me/FZXTeraboxBypassBot>𝖳𝖾𝗋𝖺𝖻𝗈𝗑 𝖡𝗒𝗉𝖺𝗌𝗌 𝖡𝗈𝗍</a>
 📚 𝖫𝗂𝖻𝗋𝖺𝗋𝗒: <a href=https://github.com/pyrogram>𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆</a>
-🚀 𝖲𝖾𝗋𝗏𝖾𝗋 : <a href=https://www.Heroku.com>Heroku</a>
+🚀 𝖲𝖾𝗋𝗏𝖾𝗋 : <a href=https://www.Heroku.com>𝖧𝖾𝗋𝗈𝗄𝗎</a>
 📝 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾: <a href=https://www.python.org>𝖯𝗒𝗍𝗁𝗈𝗇3</a>
 💾 𝖣𝖺𝗍𝖺𝖡𝖺𝗌𝖾: <a href=https://cloud.mongodb.com>𝖬𝗈𝗇𝗀𝗈𝖣𝖡</a>
 🧑‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋: <a href=https://telegram.me/MysterySD>꧁❏รเlєภt ๔є๓๏ภ❏꧂</a>
